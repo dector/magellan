9dbe2d5934bf88670b8c9150b858f1746de54991
Denys P <dector9@gmail.com>
2018-02-26 23:57:37 +0400

    Translate Log to kotlin

---
+++ core/src/com/gdxjam/magellan/Log.kt
@@ -0,0 +1,67 @@
+package com.gdxjam.magellan
+
+import com.badlogic.gdx.scenes.scene2d.Group
+import com.badlogic.gdx.scenes.scene2d.InputEvent
+import com.badlogic.gdx.scenes.scene2d.InputListener
+import com.badlogic.gdx.scenes.scene2d.ui.Label
+import com.badlogic.gdx.utils.Array
+
+/**
+ * Created by saibotd on 14.01.16.
+ */
+class Log(private val target: Group) {
+
+    private val log = Array<LogEntry>()
+
+    fun addEntry(s: String) {
+        log.insert(0, LogEntry.from(s))
+        update()
+    }
+
+    fun addEntry(s: String, sector: Sector) {
+        log.insert(0, LogEntry.from(s, sector))
+        update()
+    }
+
+    fun update() {
+        target.clear()
+
+        log.forEach { entry ->
+            val l = Label(entry.text, MagellanGame.instance.mapScreen.skin)
+                    .apply { setWrap(true) }
+
+            entry.sector?.let { sector ->
+                l.addListener(object : InputListener() {
+
+                    override fun touchDown(event: InputEvent?, x: Float, y: Float, pointer: Int, button: Int): Boolean {
+                        return true
+                    }
+
+                    override fun touchUp(event: InputEvent?, x: Float, y: Float, pointer: Int, button: Int) {
+                        if (!sector.discovered) return
+
+                        MagellanGame.instance.mapScreen.focusOnSector(sector)
+                    }
+                })
+            }
+
+            target.addActor(l)
+        }
+    }
+
+    fun render(delta: Float) {}
+}
+
+private data class LogEntry(
+        val text: String,
+        val sector: Sector? = null
+) {
+
+    companion object {
+
+        fun from(text: String, sector: Sector? = null) = LogEntry(
+                text = if (text == "") "" else "${MagellanGame.gameState.YEAR} $text",
+                sector = sector
+        )
+    }
+}
