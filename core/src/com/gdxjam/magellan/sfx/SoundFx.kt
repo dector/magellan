591c26d8ce13bddef8f9a674d9602aad0c19b5c0
Denys P <dector9@gmail.com>
2018-02-28 10:41:41 +0400

    Cleanup SoundFx file

--- core/src/com/gdxjam/magellan/sfx/SoundFx.kt
+++ core/src/com/gdxjam/magellan/sfx/SoundFx.kt
@@ -9,51 +9,29 @@ import com.gdxjam.magellan.utils.sounds
  */
 class SoundFx(assets: AssetManager) {
 
-    @JvmField
     val shipJump = assets.sound("sounds/ship_jump_2.mp3")
-
-    @JvmField
     val buy = assets.sound("sounds/buy_sell.mp3")
-
-    @JvmField
     val button = assets.sound("sounds/button.mp3")
-
-    @JvmField
     val mine = assets.sound("sounds/mine.mp3")
-
-    @JvmField
     val population = assets.sound("sounds/population.mp3")
-
-    @JvmField
     val upgrade = assets.sound("sounds/upgrade.mp3")
-
-    @JvmField
     val nope = assets.sound("sounds/nope.mp3")
-
-    @JvmField
     val doomed = assets.sound("sounds/doomed.wav")
 
-    @JvmField
     val explosions = assets.sounds(
             "sounds/explosion1.wav",
             "sounds/explosion2.wav",
             "sounds/explosion3.wav",
             "sounds/explosion4.wav"
     )
-
-    @JvmField
     val weaponFire = assets.sounds(
             "sounds/weaponfire2.wav",
             "sounds/weaponfire3.wav"
     )
-
-    @JvmField
     val weaponFireSmall = assets.sounds(
             "sounds/weaponfire5.wav",
             "sounds/weaponfire6.wav"
     )
-
-    @JvmField
     val shield = assets.sounds(
             "sounds/shield_1.mp3",
             "sounds/shield_2.mp3"
